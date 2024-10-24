9e69dba6bc92015ba3017719eebbceadc6dd63d2
Aleksey Videman <alekseyvideman@gmail.com>
2024-10-24 10:30:38 +0700

    сделал пример списка команды

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home(): JSX.Element {
           Привет! Вы на сайте сервера Videman.
         </h1>
 
-        <img src="https://i.pinimg.com/736x/6a/44/33/6a4433d2f74a809c86b8d9e55d63ef5c.jpg"/>
+        <img src="/img/joke/team.png"/>
     </Layout>
   );
 }
