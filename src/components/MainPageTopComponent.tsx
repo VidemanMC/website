e85b10f4e7591bf2d7764e3b3a4c964659c6eb4c
daradanci <daradanci@gmail.com>
2024-11-02 14:45:21 +0300

    теперь начальная страница (/mainPage) уже на что-то похожа.

--- src/components/MainPageTopComponent.tsx
+++ src/components/MainPageTopComponent.tsx
@@ -6,8 +6,8 @@ export default function MainPageTopComponent() {
     <div className='simpleWrapper'>
     <div className='mainPageTransparentOverlay'>
         <div className='mainPageTopServerTitle'>VidemanMC.ru</div>
-        <div>отдыхай, играя!</div>
-        <div>Сервер с уникальным игровым опытом, домашним уютом и дружеской атмосферой.</div>
+        <div className='mainPageTopSlogan'>отдыхай, играя!</div>
+        <div className='mainPageTopDescription'>Сервер с уникальным игровым опытом, домашним уютом и дружеской атмосферой.</div>
     </div>
     </div>
 
