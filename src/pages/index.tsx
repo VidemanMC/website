ca023a477bae5c713e6d921104bdddbebbb2497a
daradanci <daradanci@gmail.com>
2024-11-08 01:50:40 +0300

    fixed main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,18 +7,33 @@ import { Provider } from 'react-redux';
 import store from '../store/store';
 import MainPageSlider from '../components/MainPageSlider';
 import MainPageTopComponent from '../components/MainPageTopComponent';
+import { useState } from 'react';
 
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
-  
+  let audio = new Audio("/music/lillibulleroflute-77207.mp3")
+  const [auduoIsPlayed, setAudioPlayed] = useState(false);
+
+  const start = () => {
+    if (!auduoIsPlayed){
+      audio.play()
+      setAudioPlayed(true)
+    }
+    else{
+      
+    }
+  }
   return (
     <Provider store={store}>
       <Layout
         title={`${siteConfig.title}`}>
-      <div className='allInOne'>
+      <div className='allInOne'
+      onMouseDown={start}
+
+      
+      >
       
 
-      <audio src="/music/lillibulleroflute-77207.mp3" autoPlay />
       
         <MainPageTopComponent/>
         <MainPageSlider/>
