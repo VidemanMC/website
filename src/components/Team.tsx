ec25c4835fe1527b196b8f6ce37defb2371fd7c3
daradanci <daradanci@gmail.com>
2025-03-03 17:59:56 +0300

    implemented lazy loader

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -6,54 +6,56 @@ import TeammateCard from './TeammateCard';
 
 export default function Team() {
     const dispatch = useDispatch();
-    const pickedSlideElement = useSelector((state: RootState) => state.pickedSlideElement.pickedSlideElement); 
+    const pickedSlideElement = useSelector((state: RootState) => state.pickedSlideElement.pickedSlideElement);
+
     const team = [
-    {
-        name: 'WhiteLight',
-        job: 'администратор',
-        words: 'Я ваш любимый администратор.',
-        links: [],
-        img: '/img/pages/team/WhiteLight.png',
-    },
-    {
-        name: 'Wwelol08',
-        job: 'главный модпакер',
-        words: 'Я один из разработчиков модового сервера, и если вы найдёте баг, ни в коем случае не пишите о нём.',
-        links: [],
-        img: '/img/pages/team/Wwelol08.png',
-    },
-    {
-        name: 'Protosso',
-        job: 'графический дизайнер',
-        words: 'К облагорожению сайта готов!',
-        links: [],
-        img: '/img/pages/team/videman-green.jpg',
-    },
-    {
-        name: 'TallFly',
-        job: 'переводчик',
-        words: 'Отвечаю за перевод модов и плагинов. Если перевода не существует - делаю его самостоятельно (づ ◕‿◕ )づ',
-        links: [],
-        img: '/img/pages/team/TallFly.png',
-    },
-    {
-        name: 'daradanci',
-        job: 'аналитик',
-        words: 'Лягушка.',
-        links: [],
-        img: '/img/pages/team/daradanci.png',
-    },]
+        {
+            name: 'WhiteLight',
+            job: 'администратор',
+            words: 'Я ваш любимый администратор.',
+            links: [],
+            img: '/img/pages/team/WhiteLight.png',
+        },
+        {
+            name: 'Wwelol08',
+            job: 'главный модпакер',
+            words: 'Я один из разработчиков модового сервера, и если вы найдёте баг, ни в коем случае не пишите о нём.',
+            links: [],
+            img: '/img/pages/team/Wwelol08.png',
+        },
+        {
+            name: 'Protosso',
+            job: 'графический дизайнер',
+            words: 'К облагорожению сайта готов!',
+            links: [],
+            img: '/img/pages/team/videman-green.jpg',
+        },
+        {
+            name: 'TallFly',
+            job: 'переводчик',
+            words: 'Отвечаю за перевод модов и плагинов. Если перевода не существует - делаю его самостоятельно (づ ◕‿◕ )づ',
+            links: [],
+            img: '/img/pages/team/TallFly.png',
+        },
+        {
+            name: 'daradanci',
+            job: 'аналитик',
+            words: 'Лягушка.',
+            links: [],
+            img: '/img/pages/team/daradanci.png',
+        },
+    ];
 
-
-    return <div className='slideView team'>
-        <div className='slideViewTitle animatedDiv'>
-            Команда
+    return (
+        <div className="slideView team">
+            <div className="slideViewTitle animatedDiv">Команда</div>
+            <div className="teammatesContainer">
+                <div className="teammates">
+                    {team.map((teammate, index) => (
+                        <TeammateCard key={index} {...teammate} />
+                    ))}
+                </div>
+            </div>
         </div>
-        <div className='teammates'>
-            {team.map((teammate, index) =>(
-                <TeammateCard name={teammate.name} job ={ teammate.job}  words= {teammate.words}  links={teammate.links}  img={teammate.img} />
-            ))}
-        </div>
-
-    </div>;
+    );
 }
--- src/components/TeammateCard.tsx
+++ src/components/TeammateCard.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 import { setPickedSlideElement } from '../store/slice';
 import { RootState } from '../store/store';
 
-export default function TeammateCard({name, job, words, links, img}) {
+export default function TeammateCard({ name, job, words, links, img }) {
     const dispatch = useDispatch();
     const pickedSlideElement = useSelector((state: RootState) => state.pickedSlideElement.pickedSlideElement); 
-    return <div className='teammateCard'>
-        <img src={img} alt={`${name}'s photo`} className='teammateImg'/>
-        <div className='teammateInfo'>
 
-            <div className='teammateName'>
-                {name}
-            </div>
-            <div className='teammateJob'>
-                {job}
-            </div>
-            <div className='teammateWords'>
-                {words}
+    return (
+        <div className='teammateCard'>
+            <LazyLoadImage
+                src={img}
+                alt={`${name}'s photo`}
+                className='teammateImg'
+                effect="blur"
+                wrapperClassName="teammateImgWrapper"
+            />
+            <div className='teammateInfo'>
+                <div className='teammateName'>{name}</div>
+                <div className='teammateJob'>{job}</div>
+                <div className='teammateWords'>{words}</div>
             </div>
         </div>
-        
-
-    </div>;
+    );
 }
