b2afab8741ecaa6876a66f67258f8e95de5d2769
Aleksey Videman <alekseyvideman@gmail.com>
2025-06-28 13:57:47 +0700

    Add Benefits section
    Move social media link to the footer
    
    Update description of oneblock

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,94 +1,117 @@
-import { themes as prismThemes } from 'prism-react-renderer';
-import type { Config } from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'VidemanMC',
-  favicon: 'img/icon.png',
+  title: "VidemanMC",
+  favicon: "img/icon.png",
 
-  url: 'http://videmanmc.ru',
-  baseUrl: '/',
+  url: "http://videmanmc.ru",
+  baseUrl: "/",
   trailingSlash: false,
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   i18n: {
-    defaultLocale: 'ru',
-    locales: ['ru'],
+    defaultLocale: "ru",
+    locales: ["ru"],
   },
 
   scripts: [
-    { src: 'https://videmanmc.ru/scripts/yandex-analitics.js', defer: true, 'data-domain': 'videmanmc.ru' },
+    {
+      src: "https://videmanmc.ru/scripts/yandex-analitics.js",
+      defer: true,
+      "data-domain": "videmanmc.ru",
+    },
   ],
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
-        sitemap: false, // Отключение встроенного плагина sitemap
+        sitemap: false,
       } satisfies Preset.Options,
     ],
   ],
 
   themeConfig: {
     metadata: [
-      { name: 'title', content: 'VidemanMC' },
-      { name: 'keywords', content: 'minecraft, multiplayer, survival, oneblock, mods, pvp, майнкрафт, выживание, моды, ванблок, пвп' },
-      { name: 'description', content: 'сервера для игры с друзьями во множестве увлекательных режимов!' },
+      { name: "title", content: "VidemanMC" },
+      {
+        name: "keywords",
+        content: "майнкрафт, выживание, моды, ванила, взрослые игроки",
+      },
+      {
+        name: "description",
+        content:
+          "сервера для игры с друзьями во множестве увлекательных режимов!",
+      },
     ],
     navbar: {
       items: [
         {
-          to: '/donations',
-          label: 'Донаты',
-          position: 'right',
-          activeBaseRegex: 'docs/(next|v8)',
+          type: "docSidebar",
+          sidebarId: "modSidebar",
+          position: "left",
+          label: "The Cut Occult",
         },
         {
-          type: 'docSidebar',
-          sidebarId: 'modSidebar',
-          position: 'left',
-          label: 'The Cut Occult',
+          type: "docSidebar",
+          sidebarId: "survivalSidebar",
+          position: "left",
+          label: "Survival",
         },
         {
-          type: 'docSidebar',
-          sidebarId: 'oneBlockSidebar',
-          position: 'left',
-          label: 'OneBlock',
-        },
-        {
-          to: 'https://store.videmanmc.ru',
-          label: 'Магазин',
-          position: 'right',
-        },
-        {
-          to: 'https://www.youtube.com/@videmanmc',
-          label: 'Ютуб',
-          position: 'right',
-        },
-        {
-          to: 'https://t.me/+AakeNuqMt6M2NDgy',
-          label: 'Телеграм',
-          position: 'right',
+          to: "https://store.videmanmc.ru",
+          label: "Магазин",
+          position: "right",
         },
       ],
       hideOnScroll: true,
       logo: {
-        alt: 'VidemanLogo',
-        src: 'img/icon_transparet_background.png',
-        target: '_self',
+        alt: "VidemanLogo",
+        src: "img/icon_transparet_background.png",
+        target: "_self",
       },
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       copyright: `Copyright © ${new Date().getFullYear()} VidemanMC`,
+      links: [
+        {
+          title: 'Соц. сети',
+          items: [
+            {
+              label: 'Телеграм',
+              to: 'https://t.me/videmanmc',
+            },
+            {
+              label: 'Ютуб',
+              to: 'https://www.youtube.com/@videmanmc',
+            },
+          ],
+        },
+        {
+          title: 'Интересное',
+          items: [
+            {
+              label: 'Правила',
+              to: '/rules',
+            },
+            {
+              label: 'Льготы строителям',
+              to: '/builder-benefits',
+            },
+          ],
+        },
+      ],
     },
     prism: {
       theme: prismThemes.jettwaveLight,
@@ -98,16 +121,13 @@ const config: Config = {
 
   plugins: [
     [
-      './static/plugins/custom-sitemap.js',
+      "./static/plugins/custom-sitemap.js",
       {
         priorityMap: {
-          '/': { priority: 1.0, changefreq: 'daily' },
-          '/docs/mods/intro': { priority: 0.9, changefreq: 'daily' },
-          '/docs/oneblock/intro': { priority: 0.9, changefreq: 'daily' },
+          "/": { priority: 1.0, changefreq: "daily" },
+          "/docs/mods/intro": { priority: 0.9, changefreq: "daily" },
+          "/docs/survival/intro": { priority: 0.9, changefreq: "daily" },
         },
-        excludePaths: [
-          '/TestPage',
-        ],
       },
     ],
   ],
